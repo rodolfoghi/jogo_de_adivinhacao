286ed270f766ffbf3b77b63cd86a636c28e66f99
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2018-03-09 15:20:14 -0300

    Permitindo multiplos palpites

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,30 @@ fn main() {
 
     let numero_secreto = rand::thread_rng().gen_range(1, 101);
 
-    println!("O número secreto é {}", numero_secreto);
+    // println!("O número secreto é {}", numero_secreto);
 
-    println!("Digite o seu palpite.");
+    loop {
+        println!("Digite o seu palpite.");
 
-    let mut palpite = String::new();
+        let mut palpite = String::new();
 
-    io::stdin().read_line(&mut palpite)
-        .expect("Falha ao ler entrada");
+        io::stdin().read_line(&mut palpite)
+            .expect("Falha ao ler entrada");
 
-    let palpite: u32 = palpite.trim().parse()
-        .expect("Por favor, informe um número");
+        let palpite: u32 = match palpite.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("O seu palpite é {}", palpite);
+        println!("O seu palpite é {}", palpite);
 
-    match palpite.cmp(&numero_secreto) {
-        Ordering::Less => println!("Muito baixo!"),
-        Ordering::Greater => println!("Muito alto!"),
-        Ordering::Equal => println!("Você acertou!"),
+        match palpite.cmp(&numero_secreto) {
+            Ordering::Less => println!("Muito baixo!"),
+            Ordering::Greater => println!("Muito alto!"),
+            Ordering::Equal => {
+                println!("Você acertou!");
+                break;
+            }
+        }
     }
 }
