a063bfc7d28fe6fec6744251c035188664d975c6
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2018-03-09 14:34:19 -0300

    Lê um palpite do usuário e o exibe em tela

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Adivinhe o número!");
+
+    println!("Digite o seu palpite.");
+
+    let mut palpite = String::new();
+
+    io::stdin().read_line(&mut palpite)
+        .expect("Falha ao ler entrada");
+
+    println!("O seu palpite é {}", palpite);
 }
